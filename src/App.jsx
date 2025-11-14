b54638826d60bc7c4f8f32613e4db2b00057cf18
Stella40k <bernardstella0704@gmail.com>
2025-11-14 15:03:40 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //recibe como parametro el valor q queremos usar
+  const [counter, setCounter] = useState(0);
 
+  const [name, setName] = useState(["pomelo", "strella", "sese"]);
+  const [index, setIndex] = useState(0);
+  for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+
+      <h1>{name[index]}</h1>
+      <button onClick={() => setIndex((index + 1) % name.length)}>
+        siguiente
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
